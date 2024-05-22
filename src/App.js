abd009a1cda7388d381cfbb34009b967f7103264
Raman-Luhach <ramanluhach369@gmail.com>
2024-05-22 05:51:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-
+import NavBar from "./components/navBar";
+import LoginPage from "./components/signIn";
+import SignUp from "./components/signUp";
+import ShowData from "./components/pages/Body";
+import Body from "./components/pages/Body";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {useContext} from "react";
+import {ApiContext} from "./components/context/ApiContext";
+import {PrimeReactProvider} from "primereact/api";
+import CarouselSpace from "./components/CarouselSpace";
+import Video from "./components/Video";
+// import Body from "./components/pages/Body";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const router = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <NavBar />,
+                children: [
+                    {
+                        path:"/",
+                        element:<CarouselSpace/>
+                    },
+                    {
+                        path:"/login",
+                        element:<LoginPage/>
+                    },
+                    {
+                        path:"/signup",
+                        element:<SignUp/>
+                    },
+                    {
+                        path: "/video/:videoUrl",
+                        element: <Video/>
+                    }
+                ]
+            }
+        ]
+    );
+      const Apicontext = useContext(ApiContext);
+      return (
+        <div className="App bg-black">
+            <PrimeReactProvider>
+                <RouterProvider router={router}/>
+            </PrimeReactProvider>
+        </div>
+      );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        customPurple: '#9900CE',
+      },
+      borderColor: {
+        customPurple: '#9900CE',
+      },
+      backgroundColor: {
+        customPurple: '#9900CE',
+      },
+    },
   },
   plugins: [],
 }
-
